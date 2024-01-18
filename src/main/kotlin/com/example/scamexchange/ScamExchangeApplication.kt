078482df95015db444bdcfd8bf61925670ed9cd1
kimkangyun <rlarkddbs3@osstem.com>
2024-01-18 17:05:32 +0900

    init

---
+++ src/main/kotlin/com/example/scamexchange/ScamExchangeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.scamexchange
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScamExchangeApplication
+
+fun main(args: Array<String>) {
+    runApplication<ScamExchangeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/scamexchange/ScamExchangeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.scamexchange
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScamExchangeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
